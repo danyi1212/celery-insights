12a8a20fa81a8e03ddaa90826b9b59b3db190b35
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-11 03:02:03 +0200

    Added material ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {ThemeProvider} from '@emotion/react';
+import {CssBaseline} from '@mui/material';
+import theme from './theme';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
+);
---
+++ frontend/src/theme.ts
@@ -0,0 +1,11 @@
+import {createTheme} from '@mui/material/styles';
+import {red} from '@mui/material/colors';
+
+// A custom theme for this app
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+    }
+});
+
+export default theme;
