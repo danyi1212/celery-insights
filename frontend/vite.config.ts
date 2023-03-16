9192bdfd3b46f27199ab66058be65f7cd229a32c
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-16 18:37:16 +0200

    Added vite reverse proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,39 @@
-import react from '@vitejs/plugin-react-swc'
-import {defineConfig} from 'vite'
+import react from "@vitejs/plugin-react-swc"
+import {defineConfig} from "vite"
 import eslint from "vite-plugin-eslint"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [eslint(), tsconfigPaths(), react()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:8555",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/ws": {
+                target: "ws://localhost:8555",
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+            },
+            "/docs": {
+                target: "http://localhost:8555",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/redoc": {
+                target: "http://localhost:8555",
+                changeOrigin: true,
+                secure: false,
+            },
+            "/openapi.json": {
+                target: "http://localhost:8555",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 })
