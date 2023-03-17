1c40dcc7cec56fd63be7e3784d3a9bd3a08f668c
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-18 01:57:14 +0200

    Changed ws url to relative (local using vite reverse proxy)

--- frontend/src/components/CeleryStateSync.tsx
+++ frontend/src/components/CeleryStateSync.tsx
@@ -6,8 +6,14 @@ import {
 import React, { useEffect } from "react"
 import useWebSocket from "react-use-websocket"
 
+const toWsUri = (path: string): string => {
+    const location = window.location
+    const protocol = location.protocol === "https:" ? "wss:" : "ws:"
+    return `${protocol}//${location.host}/${path}`
+}
+
 const CeleryStateSync: React.FC = () => {
-    const { readyState } = useWebSocket("ws://localhost:8555/ws/events", {
+    const { readyState } = useWebSocket(toWsUri("ws/events"), {
         shouldReconnect: () => true,
         onOpen: () => console.log("Connected to websockets!"),
         onClose: () => console.log("Disconnected from websockets!"),
