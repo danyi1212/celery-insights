dca54ea629f13c6679f9d988108cd1d355a7cc59
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-18 16:00:42 +0200

    Fixed worker active filter

--- server/workers/router.py
+++ server/workers/router.py
@@ -16,7 +16,7 @@ def get_workers(alive: bool | None = None) -> list[Worker]:
     return [
         Worker.from_celery_worker(worker)
         for worker in state.workers.itervalues()
-        if alive is not None and worker.alive == alive
+        if alive is None or worker.alive == alive
 
     ]
 
