68bdc3e9fcf8f459c68bd6dd0ff6b01f1ef09c16
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-19 18:45:56 +0200

    Fixed typo

--- server/workers/router.py
+++ server/workers/router.py
@@ -21,43 +21,43 @@ def get_workers(alive: bool | None = None) -> list[Worker]:
     ]
 
 
-@workers_router.get("/workers/stats", description="Worker Statistics")
+@workers_router.get("/stats", description="Worker Statistics")
 @cache(expire=30)
 async def get_worker_stats(inspect: Inspect = Depends(get_inspect)) -> dict[str, Stats]:
     return await asyncio.to_thread(inspect.stats)
 
 
-@workers_router.get("/workers/registered", description="Worker Registered Task Types")
+@workers_router.get("/registered", description="Worker Registered Task Types")
 @cache(expire=30)
 async def get_worker_registered(inspect: Inspect = Depends(get_inspect)) -> dict[str, list[str]]:
     return await asyncio.to_thread(inspect.registered)
 
 
-@workers_router.get("/workers/revoked", description="Worker Revoked Tasks list")
+@workers_router.get("/revoked", description="Worker Revoked Tasks list")
 @cache(expire=30)
 async def get_worker_revoked(inspect: Inspect = Depends(get_inspect)) -> dict[str, list[str]]:
     return await asyncio.to_thread(inspect.revoked)
 
 
-@workers_router.get("/workers/scheduled", description="Worker Scheduled Tasks (eta / countdown)")
+@workers_router.get("/scheduled", description="Worker Scheduled Tasks (eta / countdown)")
 @cache(expire=30)
 async def get_worker_scheduled(inspect: Inspect = Depends(get_inspect)) -> dict[str, list[str]]:
     return await asyncio.to_thread(inspect.scheduled)
 
 
-@workers_router.get("/workers/reserved", description="Worker Prefetched Tasks")
+@workers_router.get("/reserved", description="Worker Prefetched Tasks")
 @cache(expire=30)
 async def get_worker_reserved(inspect: Inspect = Depends(get_inspect)) -> dict[str, list[str]]:
     return await asyncio.to_thread(inspect.reserved)
 
 
-@workers_router.get("/workers/active", description="Worker currently executing tasks")
+@workers_router.get("/active", description="Worker currently executing tasks")
 @cache(expire=30)
 async def get_worker_active(inspect: Inspect = Depends(get_inspect)) -> dict[str, list[str]]:
     return await asyncio.to_thread(inspect.active)
 
 
-@workers_router.get("/workers/queues", description="Worker active consumer queues")
+@workers_router.get("/queues", description="Worker active consumer queues")
 @cache(expire=30)
 async def get_worker_queues(inspect: Inspect = Depends(get_inspect)) -> dict[str, list[QueueInfo]]:
     return await asyncio.to_thread(inspect.active_queues)
