b825a1a1f2a7ef566f3f95240b1f250410c1979c
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-19 23:41:13 +0200

    Added last update column to explorer

--- frontend/src/pages/ExplorerPage.tsx
+++ frontend/src/pages/ExplorerPage.tsx
@@ -7,9 +7,16 @@ import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
 import { DataGrid, GridColDef } from "@mui/x-data-grid"
 import { useStateStore } from "@stores/useStateStore"
+import { format } from "date-fns"
 import React, { useState } from "react"
 
 const columns: GridColDef[] = [
+    {
+        field: "lastUpdated",
+        headerName: "Last Updated",
+        minWidth: 160,
+        valueFormatter: (params) => format(params.value, "MMM dd  hh:mm:ss.SSS"),
+    },
     { field: "id", headerName: "Task ID", width: 100 },
     { field: "type", headerName: "Task Type", width: 300 },
 ]
@@ -40,7 +47,12 @@ const ExplorerPage: React.FC = () => {
                         <Box flexGrow={1} />
                         <Typography variant="subtitle2">{tasks.length} Tasks found</Typography>
                     </Toolbar>
-                    <DataGrid columns={columns} rows={tasks} autoHeight />
+                    <DataGrid
+                        columns={columns}
+                        rows={tasks}
+                        initialState={{ sorting: { sortModel: [{ field: "lastUpdated", sort: "desc" }] } }}
+                        autoHeight
+                    />
                 </Box>
             </Box>
         </Box>
