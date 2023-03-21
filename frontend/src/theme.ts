8911d8e6f981b674ab6fd26ece24918e88033658
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-21 19:42:54 +0200

    Added dark scrollbars

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,9 +1,17 @@
+import darkScrollbar from "@mui/material/darkScrollbar"
 import {createTheme} from "@mui/material/styles"
 
 // A custom theme for this app
 const theme = createTheme({
     palette: {
         mode: "dark",
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: (themeParam) => ({
+                body: themeParam.palette.mode === "dark" ? darkScrollbar() : null,
+            }),
+        },
     },
 })
 
