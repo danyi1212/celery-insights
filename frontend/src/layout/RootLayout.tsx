bcc9328526a38c55a79dfd91eba735c6c7091ea8
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-22 00:12:56 +0200

    Changed main layout to fill screen

--- frontend/src/layout/RootLayout.tsx
+++ frontend/src/layout/RootLayout.tsx
@@ -2,7 +2,6 @@ import ConsolidatedProviders from "@layout/ConsolidatedProviders"
 import Header from "@layout/Header"
 import Menu from "@layout/Menu"
 import Box from "@mui/material/Box"
-import Container from "@mui/material/Container"
 import Toolbar from "@mui/material/Toolbar"
 import React from "react"
 import { Outlet } from "react-router-dom"
@@ -15,9 +14,7 @@ const RootLayout: React.FC = () => {
                 <Box component="main" flexGrow="1" minHeight="100vh" overflow="auto">
                     <Header />
                     <Toolbar />
-                    <Container maxWidth="xl">
-                        <Outlet />
-                    </Container>
+                    <Outlet />
                 </Box>
             </Box>
         </ConsolidatedProviders>
