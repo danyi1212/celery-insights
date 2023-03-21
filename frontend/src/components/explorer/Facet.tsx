8b648022e50cbd7aa6ecc4e2dd0888511fd3b1e1
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-22 01:24:18 +0200

    Extracted facet value to component

--- frontend/src/components/explorer/Facet.tsx
+++ frontend/src/components/explorer/Facet.tsx
@@ -1,18 +1,13 @@
 import FacetQuickFilter from "@components/explorer/FacetQuickFilter"
+import FacetValue from "@components/explorer/FacetValue"
 import ClearAllIcon from "@mui/icons-material/ClearAll"
 import ExpandLessIcon from "@mui/icons-material/ExpandLess"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 import Box from "@mui/material/Box"
-import Checkbox from "@mui/material/Checkbox"
 import Collapse from "@mui/material/Collapse"
 import Divider from "@mui/material/Divider"
 import IconButton from "@mui/material/IconButton"
 import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemIcon from "@mui/material/ListItemIcon"
-import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction"
-import ListItemText from "@mui/material/ListItemText"
 import Tooltip from "@mui/material/Tooltip"
 import Typography from "@mui/material/Typography"
 import React, { useState } from "react"
@@ -67,30 +62,13 @@ const Facet: React.FC<FacetProps> = ({ title, counts, selected, setSelected }) =
                             .filter(([value]) => !filter || value.toLowerCase().includes(filter.toLowerCase()))
                             .sort((a, b) => b[1] - a[1])
                             .map(([value, count]) => (
-                                <ListItem key={value} dense disablePadding>
-                                    <ListItemButton dense sx={{ p: 0, pl: 2 }} onClick={() => handleSelect(value)}>
-                                        <ListItemIcon sx={{ minWidth: 0 }}>
-                                            <Checkbox
-                                                edge="start"
-                                                tabIndex={-1}
-                                                checked={selected.size === 0 || selected.has(value)}
-                                            />
-                                        </ListItemIcon>
-                                        <Tooltip title={value} placement="right" arrow>
-                                            <ListItemText
-                                                primary={value}
-                                                primaryTypographyProps={{
-                                                    variant: "caption",
-                                                    maxWidth: "80%",
-                                                    noWrap: true,
-                                                }}
-                                            />
-                                        </Tooltip>
-                                        <ListItemSecondaryAction>
-                                            <Typography>{count}</Typography>
-                                        </ListItemSecondaryAction>
-                                    </ListItemButton>
-                                </ListItem>
+                                <FacetValue
+                                    key={value}
+                                    value={value}
+                                    count={count}
+                                    selected={selected}
+                                    onSelect={() => handleSelect(value)}
+                                />
                             ))}
                     </List>
                 </Box>
---
+++ frontend/src/components/explorer/FacetValue.tsx
@@ -0,0 +1,40 @@
+import Checkbox from "@mui/material/Checkbox"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemSecondaryAction from "@mui/material/ListItemSecondaryAction"
+import ListItemText from "@mui/material/ListItemText"
+import Tooltip from "@mui/material/Tooltip"
+import Typography from "@mui/material/Typography"
+import React from "react"
+
+interface FacetValueProps {
+    value: string
+    count: number
+    onSelect: () => void
+    selected: Set<string>
+}
+
+const FacetValue: React.FC<FacetValueProps> = ({ value, count, selected, onSelect }) => (
+    <ListItem dense disablePadding>
+        <ListItemButton dense sx={{ p: 0, pl: 2 }} onClick={onSelect}>
+            <ListItemIcon sx={{ minWidth: 0 }}>
+                <Checkbox edge="start" tabIndex={-1} checked={selected.size === 0 || selected.has(value)} />
+            </ListItemIcon>
+            <Tooltip title={value} placement="right" arrow>
+                <ListItemText
+                    primary={value}
+                    primaryTypographyProps={{
+                        variant: "caption",
+                        maxWidth: "80%",
+                        noWrap: true,
+                    }}
+                />
+            </Tooltip>
+            <ListItemSecondaryAction>
+                <Typography>{count}</Typography>
+            </ListItemSecondaryAction>
+        </ListItemButton>
+    </ListItem>
+)
+export default FacetValue
