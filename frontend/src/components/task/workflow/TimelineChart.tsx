727b09955b81b8a1bbcf3388f8968fb45a409558
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-24 19:06:25 +0300

    Changed timestamp now to UTC time

--- frontend/src/components/task/workflow/TimelineChart.tsx
+++ frontend/src/components/task/workflow/TimelineChart.tsx
@@ -1,6 +1,56 @@
-import Box from "@mui/material/Box"
+import { Theme, useTheme } from "@mui/material"
 import { StateTask } from "@utils/translateServerModels"
-import React from "react"
+import { ApexOptions } from "apexcharts"
+import { formatDistanceStrict } from "date-fns"
+import React, { useMemo } from "react"
+import Chart from "react-apexcharts"
+
+const getTimestamp = (date: Date | undefined) => date?.getTime() || new Date().getTime()
+
+const getSeries = (tasks: StateTask[]): ApexAxisChartSeries => {
+    const data = tasks
+        .sort((a, b) => ((a.sentAt || a.lastUpdated) > (b.sentAt || a.lastUpdated) ? 1 : -1))
+        .map((task) => ({
+            x: task.id,
+            y: [getTimestamp(task.startedAt), getTimestamp(task.succeededAt || task.failedAt)],
+        }))
+    console.log(data[data.length - 1])
+    return [
+        {
+            data,
+        },
+    ]
+}
+
+const getOptions = (theme: Theme): ApexOptions => ({
+    chart: {
+        background: theme.palette.background.paper,
+        foreColor: theme.palette.text.primary,
+        fontFamily: theme.typography.fontFamily,
+    },
+    plotOptions: {
+        bar: {
+            horizontal: true,
+            distributed: true,
+            dataLabels: {
+                hideOverflowingLabels: true,
+            },
+        },
+    },
+    dataLabels: {
+        enabled: true,
+        formatter: (value) => {
+            const [first, second] = value as [number, number]
+            return formatDistanceStrict(first, second)
+        },
+    },
+    xaxis: { type: "datetime" },
+    yaxis: {
+        labels: {
+            formatter: (value) => value.toString().slice(0, 5),
+        },
+    },
+})
 
 interface TimelineChartProps {
     tasks: StateTask[]
@@ -8,8 +58,11 @@ interface TimelineChartProps {
     currentTaskId?: string
 }
 
-const TimelineChart: React.FC<TimelineChartProps> = ({}) => {
-    return <Box bgcolor="white" height="100%"></Box>
+const TimelineChart: React.FC<TimelineChartProps> = ({ tasks }) => {
+    const theme = useTheme()
+    const series = useMemo(() => getSeries(tasks), [tasks])
+    const options: ApexOptions = useMemo(() => getOptions(theme), [theme])
+    return <Chart type="rangeBar" options={options} series={series} height="100%" width="100%" />
 }
 
 export default TimelineChart
