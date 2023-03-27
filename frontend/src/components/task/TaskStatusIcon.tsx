cb38422cf74d9a2799aa0bebf051f47c38681eae
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-27 21:16:11 +0300

    Added tooltip to task state icon

--- frontend/src/components/task/TaskStatusIcon.tsx
+++ frontend/src/components/task/TaskStatusIcon.tsx
@@ -2,42 +2,43 @@ import CancelIcon from "@mui/icons-material/Cancel"
 import ChangeCircleIcon from "@mui/icons-material/ChangeCircle"
 import CheckCircleIcon from "@mui/icons-material/CheckCircle"
 import ErrorIcon from "@mui/icons-material/Error"
-import HelpIcon from "@mui/icons-material/Help"
 import PlayCircleIcon from "@mui/icons-material/PlayCircle"
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle"
 import WatchLaterIcon from "@mui/icons-material/WatchLater"
 import { SvgIconProps } from "@mui/material"
+import Tooltip from "@mui/material/Tooltip"
 import { TaskState } from "@services/server"
 import React from "react"
 
-interface TaskStatusIconProps {
+interface TaskStatusIconProps extends SvgIconProps {
     status: TaskState
-    color?: SvgIconProps["color"]
 }
 
-const TaskStatusIcon: React.FC<TaskStatusIconProps> = ({ status, color }) => {
-    switch (status) {
-        case TaskState.PENDING:
-            return <WatchLaterIcon color={color || "disabled"} />
-        case TaskState.RECEIVED:
-            return <WatchLaterIcon color={color || "info"} />
-        case TaskState.STARTED:
-            return <PlayCircleIcon color={color || "info"} />
-        case TaskState.SUCCESS:
-            return <CheckCircleIcon color={color || "success"} />
-        case TaskState.FAILURE:
-            return <ErrorIcon color={color || "error"} />
-        case TaskState.IGNORED:
-            return <CancelIcon color={color || "error"} />
-        case TaskState.REJECTED:
-            return <CancelIcon color={color || "error"} />
-        case TaskState.REVOKED:
-            return <RemoveCircleIcon color={color || "warning"} />
-        case TaskState.RETRY:
-            return <ChangeCircleIcon color={color || "warning"} />
-        default:
-            return <HelpIcon color={color || "disabled"} />
-    }
+interface StateIconMeta {
+    icon: React.ElementType
+    color: SvgIconProps["color"]
+    tooltip: string
+}
+
+const stateMeta: Record<TaskState, StateIconMeta> = {
+    [TaskState.PENDING]: { icon: WatchLaterIcon, color: "disabled", tooltip: "Pending" },
+    [TaskState.RECEIVED]: { icon: WatchLaterIcon, color: "info", tooltip: "Received" },
+    [TaskState.STARTED]: { icon: PlayCircleIcon, color: "info", tooltip: "Started" },
+    [TaskState.SUCCESS]: { icon: CheckCircleIcon, color: "success", tooltip: "Success" },
+    [TaskState.FAILURE]: { icon: ErrorIcon, color: "error", tooltip: "Failure" },
+    [TaskState.IGNORED]: { icon: CancelIcon, color: "error", tooltip: "Ignored" },
+    [TaskState.REJECTED]: { icon: CancelIcon, color: "error", tooltip: "Rejected" },
+    [TaskState.REVOKED]: { icon: RemoveCircleIcon, color: "warning", tooltip: "Revoked" },
+    [TaskState.RETRY]: { icon: ChangeCircleIcon, color: "warning", tooltip: "Retry" },
+}
+
+const TaskStatusIcon: React.FC<TaskStatusIconProps> = ({ status, ...props }) => {
+    const meta = stateMeta[status]
+    return (
+        <Tooltip title={meta.tooltip} arrow>
+            <meta.icon color={meta.color} {...props} />
+        </Tooltip>
+    )
 }
 
 export default TaskStatusIcon
