ea4b25a8176da10295a9c228ba8ae9e3825db394
Dan Yishai <danyi1212@users.noreply.github.com>
2023-03-27 23:04:22 +0300

    Extracted task page header to component

---
+++ frontend/src/components/task/TaskPageHeader.tsx
@@ -0,0 +1,52 @@
+import TaskAvatar from "@components/task/TaskAvatar"
+import { WorkflowChartType } from "@components/task/WorkflowGraph"
+import AccountTreeIcon from "@mui/icons-material/AccountTree"
+import ViewTimelineIcon from "@mui/icons-material/ViewTimeline"
+import Box from "@mui/material/Box"
+import Stack from "@mui/material/Stack"
+import ToggleButton from "@mui/material/ToggleButton"
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup"
+import Toolbar from "@mui/material/Toolbar"
+import Tooltip from "@mui/material/Tooltip"
+import Typography from "@mui/material/Typography"
+import { StateTask } from "@utils/translateServerModels"
+import React from "react"
+
+interface TaskPageHeaderProps {
+    task: StateTask
+    chartType: WorkflowChartType
+    setChartType: (type: WorkflowChartType) => void
+}
+
+const TaskPageHeader: React.FC<TaskPageHeaderProps> = ({ task, chartType, setChartType }) => {
+    return (
+        <Toolbar>
+            <Box pr={3}>
+                <TaskAvatar taskId={task.id} type={task.type} status={task.state} />
+            </Box>
+            <Stack height={64} justifyContent="flex-end">
+                <Typography variant="h5">{task.type}</Typography>
+                <Typography variant="caption">{task.id}</Typography>
+            </Stack>
+            <Box flexGrow={1} />
+            <ToggleButtonGroup
+                value={chartType}
+                onChange={(_, newValue) => newValue && setChartType(newValue)}
+                exclusive
+                size="small"
+            >
+                <ToggleButton value={WorkflowChartType.FLOWCHART}>
+                    <Tooltip title="Flowchart">
+                        <AccountTreeIcon />
+                    </Tooltip>
+                </ToggleButton>
+                <ToggleButton value={WorkflowChartType.TIMELINE}>
+                    <Tooltip title="Timeline">
+                        <ViewTimelineIcon />
+                    </Tooltip>
+                </ToggleButton>
+            </ToggleButtonGroup>
+        </Toolbar>
+    )
+}
+export default TaskPageHeader
--- frontend/src/pages/TaskPage.tsx
+++ frontend/src/pages/TaskPage.tsx
@@ -1,16 +1,9 @@
-import TaskAvatar from "@components/task/TaskAvatar"
+import TaskPageHeader from "@components/task/TaskPageHeader"
 import WorkflowGraph, { WorkflowChartType } from "@components/task/WorkflowGraph"
 import useTaskResult from "@hooks/useTaskResult"
 import useTaskState from "@hooks/useTaskState"
-import AccountTreeIcon from "@mui/icons-material/AccountTree"
-import ViewTimelineIcon from "@mui/icons-material/ViewTimeline"
 import Box from "@mui/material/Box"
 import CircularProgress from "@mui/material/CircularProgress"
-import Stack from "@mui/material/Stack"
-import ToggleButton from "@mui/material/ToggleButton"
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup"
-import Toolbar from "@mui/material/Toolbar"
-import Tooltip from "@mui/material/Tooltip"
 import Typography from "@mui/material/Typography"
 import React from "react"
 import { useParams } from "react-router-dom"
@@ -41,33 +34,7 @@ const TaskPage: React.FC = () => {
             <Box width="100%" height="450px">
                 <WorkflowGraph chartType={chartType} rootTaskId={task.rootId || task.id} currentTaskId={task.id} />
             </Box>
-            <Toolbar>
-                <Box pr={3}>
-                    <TaskAvatar taskId={task.id} type={task.type} status={task.state} />
-                </Box>
-                <Stack height={64} justifyContent="flex-end">
-                    <Typography variant="h5">{task.type}</Typography>
-                    <Typography variant="caption">{task.id}</Typography>
-                </Stack>
-                <Box flexGrow={1} />
-                <ToggleButtonGroup
-                    value={chartType}
-                    onChange={(_, newValue) => newValue && setChartType(newValue)}
-                    exclusive
-                    size="small"
-                >
-                    <ToggleButton value={WorkflowChartType.FLOWCHART}>
-                        <Tooltip title="Flowchart">
-                            <AccountTreeIcon />
-                        </Tooltip>
-                    </ToggleButton>
-                    <ToggleButton value={WorkflowChartType.TIMELINE}>
-                        <Tooltip title="Timeline">
-                            <ViewTimelineIcon />
-                        </Tooltip>
-                    </ToggleButton>
-                </ToggleButtonGroup>
-            </Toolbar>
+            <TaskPageHeader task={task} chartType={chartType} setChartType={setChartType} />
             <Typography component="pre" overflow="auto">
                 {JSON.stringify(task, null, 2)}
             </Typography>
