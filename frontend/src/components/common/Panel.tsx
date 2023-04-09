be8806805d5063529e1e02b736e705206d18f0b7
Dan Yishai <danyi1212@users.noreply.github.com>
2023-04-09 21:58:59 +0300

    Added title props to panel

--- frontend/src/components/common/Panel.tsx
+++ frontend/src/components/common/Panel.tsx
@@ -1,4 +1,4 @@
-import { PaperProps } from "@mui/material"
+import { PaperProps, TypographyProps } from "@mui/material"
 import Paper from "@mui/material/Paper"
 import Stack from "@mui/material/Stack"
 import Toolbar from "@mui/material/Toolbar"
@@ -7,15 +7,16 @@ import React from "react"
 
 interface PanelProps extends PaperProps {
     title: string
+    titleProps?: TypographyProps
     actions?: React.ReactNode
     children?: React.ReactNode
 }
 
-const Panel: React.FC<PanelProps> = ({ title, children, actions, ...props }) => {
+const Panel: React.FC<PanelProps> = ({ title, children, actions, titleProps, ...props }) => {
     return (
         <Stack direction="column" height="100%">
             <Toolbar>
-                <Typography variant="h4" noWrap flexGrow={1}>
+                <Typography variant="h4" {...titleProps} noWrap flexGrow={1}>
                     {title}
                 </Typography>
                 {actions}
