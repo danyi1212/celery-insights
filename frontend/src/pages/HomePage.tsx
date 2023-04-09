2487e5fc4897fbe44d8a033177e1db1bac054f16
Dan Yishai <danyi1212@users.noreply.github.com>
2023-04-09 19:54:01 +0300

    Moved recent tasks to component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,31 +1,8 @@
-import TaskAvatar from "@components/task/TaskAvatar"
-import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
-import ListItemAvatar from "@mui/material/ListItemAvatar"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemText from "@mui/material/ListItemText"
-import { useStateStore } from "@stores/useStateStore"
-import React, { useMemo } from "react"
-import { Link } from "react-router-dom"
+import RecentTasksList from "@components/task/RecentTasksList"
+import React from "react"
 
 function HomePage() {
-    const tasks = useStateStore((state) => state.tasks)
-    const sortedTasks = useMemo(() => tasks.map((t) => t).sort((a, b) => (a.sentAt > b.sentAt ? -1 : 1)), [tasks])
-
-    return (
-        <List>
-            {sortedTasks.map((task) => (
-                <ListItem key={task.id} disablePadding>
-                    <ListItemButton component={Link} to={`/tasks/${task.id}`}>
-                        <ListItemAvatar>
-                            <TaskAvatar taskId={task.id} type={task.type} status={task.state} disableLink />
-                        </ListItemAvatar>
-                        <ListItemText primary={task.type} secondary={task.worker} />
-                    </ListItemButton>
-                </ListItem>
-            ))}
-        </List>
-    )
+    return <RecentTasksList />
 }
 
 export default HomePage
