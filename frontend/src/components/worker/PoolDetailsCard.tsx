a9884e600ba1f8123217c12942a621960b3c7ef6
Dan Yishai <danyi1212@users.noreply.github.com>
2023-04-10 21:33:59 +0300

    Added process pool list

--- frontend/src/components/worker/PoolDetailsCard.tsx
+++ frontend/src/components/worker/PoolDetailsCard.tsx
@@ -1,7 +1,10 @@
 import DetailItem from "@components/common/DetailItem"
 import Panel from "@components/common/Panel"
 import useWorkerState from "@hooks/useWorkerState"
+import MemoryIcon from "@mui/icons-material/Memory"
+import Chip from "@mui/material/Chip"
 import Grid from "@mui/material/Grid"
+import Tooltip from "@mui/material/Tooltip"
 import { formatSecondsDuration } from "@utils/formatSecondsDuration"
 import { StateWorker } from "@utils/translateServerModels"
 import React from "react"
@@ -34,6 +37,17 @@ const PoolDetailsCard: React.FC<PoolDetailsCardProps> = ({ worker }) => {
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <DetailItem label="Hard Timeout" value={formatSecondsDuration(stats?.pool.timeouts[1] || 0)} />
+                </Grid>
+                <Grid item xs={12}>
+                    <Grid container direction="row" spacing={0.5}>
+                        {stats?.pool.processes.map((process) => (
+                            <Grid item key={process} xs={3}>
+                                <Tooltip title="Child Process ID">
+                                    <Chip label={process} variant="outlined" icon={<MemoryIcon />} />
+                                </Tooltip>
+                            </Grid>
+                        ))}
+                    </Grid>
                 </Grid>
             </Grid>
         </Panel>
