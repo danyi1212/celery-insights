12823d3643a56f2f0c02fbdceb494333ae700a88
Dan Yishai <danyi1212@users.noreply.github.com>
2023-04-10 22:24:36 +0300

    Reduced worker stats refetch interval

--- frontend/src/hooks/useWorkerState.ts
+++ frontend/src/hooks/useWorkerState.ts
@@ -8,7 +8,7 @@ const useWorkerState = (worker: StateWorker, timeout?: number) => {
         () => new ServerClient().workers.getWorkerStats(timeout, worker?.hostname),
         [worker, timeout]
     )
-    const result = useQuery(["workers/stats", worker.hostname], getWorkerStats)
+    const result = useQuery(["workers/stats", worker.hostname], getWorkerStats, { refetchInterval: 30000 })
 
     return { ...result, stats: result.data?.[worker.hostname] }
 }
