1222a15ea9e9d56788ab7328bee0ac13cf1df2e7
Dan Yishai <danyi1212@users.noreply.github.com>
2023-04-13 19:08:16 +0300

    Changed zip to strict mode

--- server/ws/websocket_manager.py
+++ server/ws/websocket_manager.py
@@ -28,6 +28,6 @@ class WebsocketManager:
                 for connection in self.active_connections
             ], return_exceptions=True
             )
-        for result, connection in zip(results, self.active_connections):
+        for result, connection in zip(results, self.active_connections, strict=True):
             if isinstance(result, Exception):
                 logger.exception(f"Failed to send message to client {connection.client!r}: {result}", exc_info=result)
