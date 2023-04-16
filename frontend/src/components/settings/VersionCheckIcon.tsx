0b288fc317f2d784247dd2a50035e99f057b64fe
Dan Yishai <danyi1212@users.noreply.github.com>
2023-04-16 04:04:58 +0300

    Added version check against GitHub releases

--- frontend/src/components/settings/VersionCheckIcon.tsx
+++ frontend/src/components/settings/VersionCheckIcon.tsx
@@ -1,33 +1,25 @@
+import { useGithubLatestRelease } from "@hooks/useGithubLatestRelease"
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline"
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline"
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline"
 import { CircularProgressProps, SvgIconProps } from "@mui/material"
 import CircularProgress from "@mui/material/CircularProgress"
 import Tooltip from "@mui/material/Tooltip"
-import React, { useCallback } from "react"
-import { useQuery } from "react-query"
+import React, { useMemo } from "react"
+import semver from "semver"
 
 interface VersionCheckIconProps extends SvgIconProps {
     currentVersion: string | undefined
     progressProps?: CircularProgressProps
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const isUpdateAvailable = (currentVersion: string | undefined): boolean => false
-
 const VersionCheckIcon: React.FC<VersionCheckIconProps> = ({ currentVersion, progressProps, ...props }) => {
-    const { data, isLoading, error } = useQuery(
-        ["version-check", currentVersion],
-        useCallback(() => isUpdateAvailable(currentVersion), [currentVersion]),
-        { refetchInterval: 60 }
+    const { data, isLoading, error } = useGithubLatestRelease()
+    const isUpdateAvailable = useMemo(
+        () => data?.data.tag_name && currentVersion && semver.gt(data.data.tag_name, currentVersion),
+        [data?.data.tag_name, currentVersion]
     )
-    if (currentVersion === undefined)
-        return (
-            <Tooltip title="Unable to check for updates">
-                <HelpOutlineIcon color="disabled" {...props} />
-            </Tooltip>
-        )
-    else if (isLoading)
+    if (isLoading)
         return (
             <Tooltip title="Checking for updates">
                 <CircularProgress {...progressProps} />
@@ -39,9 +31,15 @@ const VersionCheckIcon: React.FC<VersionCheckIconProps> = ({ currentVersion, pro
                 <ErrorOutlineIcon color="error" {...props} />
             </Tooltip>
         )
-    else if (data) {
+    if (currentVersion === undefined || data?.data.tag_name === undefined)
         return (
-            <Tooltip title="Update is available!">
+            <Tooltip title="Unable to check for updates">
+                <HelpOutlineIcon color="disabled" {...props} />
+            </Tooltip>
+        )
+    else if (isUpdateAvailable) {
+        return (
+            <Tooltip title={`Update is available! ${currentVersion} => ${data.data.tag_name}`}>
                 <ErrorOutlineIcon color="warning" {...props} />
             </Tooltip>
         )
---
+++ frontend/src/hooks/useGithubLatestRelease.tsx
@@ -0,0 +1,104 @@
+import axios from "axios"
+import { useQuery } from "react-query"
+
+interface GitHubRelease {
+    url: string
+    html_url: string
+    assets_url: string
+    upload_url: string
+    tarball_url: string | null
+    zipball_url: string | null
+    id: number
+    node_id: string
+    tag_name: string
+    target_commitish: string
+    name: string | null
+    body: string | null
+    draft: boolean
+    prerelease: boolean
+    created_at: string
+    published_at: string | null
+    author: {
+        name: string | null
+        email: string | null
+        login: string
+        id: number
+        node_id: string
+        avatar_url: string
+        gravatar_id: string | null
+        url: string
+        html_url: string
+        followers_url: string
+        following_url: string
+        gists_url: string
+        starred_url: string
+        subscriptions_url: string
+        organizations_url: string
+        repos_url: string
+        events_url: string
+        received_events_url: string
+        type: string
+        site_admin: boolean
+        starred_at: string | null
+    }
+    assets: {
+        url: string
+        browser_download_url: string
+        id: number
+        node_id: string
+        name: string
+        label: string | null
+        state: "uploaded" | "open"
+        content_type: string
+        size: number
+        download_count: number
+        created_at: string
+        updated_at: string
+        uploader: {
+            name: string | null
+            email: string | null
+            login: string
+            id: number
+            node_id: string
+            avatar_url: string
+            gravatar_id: string | null
+            url: string
+            html_url: string
+            followers_url: string
+            following_url: string
+            gists_url: string
+            starred_url: string
+            subscriptions_url: string
+            organizations_url: string
+            repos_url: string
+            events_url: string
+            received_events_url: string
+            type: string
+            site_admin: boolean
+            starred_at: string | null
+        } | null
+    }[]
+    body_html: string
+    body_text: string
+    mentions_count: number
+    discussion_url: string
+    reactions: {
+        url: string
+        total_count: number
+        "+1": number
+        "-1": number
+        laugh: number
+        confused: number
+        heart: number
+        hooray: number
+        eyes: number
+        rocket: number
+    }
+}
+
+const getLatestRelease = () =>
+    axios.get<GitHubRelease>("https://api.github.com/repos/danyi1212/celery-insights/releases/latest")
+export const useGithubLatestRelease = () =>
+    useQuery(["latest-release"], getLatestRelease, {
+        refetchInterval: 1000 * 60 * 15, // Every 15 minutes
+    })
