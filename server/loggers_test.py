08392e1fd0d588186c0e620e28702d5b189bfa29
Dan Yishai <danyi1212@users.noreply.github.com>
2023-10-05 20:44:53 +0300

    Changed logging configuration

---
+++ server/loggers_test.py
@@ -0,0 +1,17 @@
+import logging
+
+from logging_config import LOGGING_CONFIG
+
+
+def test_all_loggers_are_configured():
+    from app import app
+
+    available_root_loggers = {
+        logger_name for logger_name in logging.root.manager.loggerDict.keys()
+        if "." not in logger_name
+    }
+    configured_loggers = LOGGING_CONFIG["loggers"].keys()
+
+    missing_loggers = available_root_loggers - configured_loggers
+
+    assert not missing_loggers, f"Missing logging configuration for {len(missing_loggers)} loggers"
--- server/logging_config.py
+++ server/logging_config.py
@@ -1,27 +1,52 @@
-from settings import Settings
-
 LOGGING_CONFIG = {
     "version": 1,
-    "disable_existing_loggers": False,
+    "disable_existing_loggers": True,
     "formatters": {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "[%(asctime)s.%(msecs)03d] %(levelname)s - %(module)s:%(lineno)d | %(message)s",
+        "color": {
+            "()": "colorlog.ColoredFormatter",
+            "format": "%(log_color)s[%(asctime)s.%(msecs)03d] %(levelname)s - %(name)s:%(lineno)d | %(message)s",
             'datefmt': "%H:%M:%S",
+            "log_colors": {
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red,bg_white",
+            },
         },
     },
     "handlers": {
-        "default": {
-            "formatter": "default",
+        "console": {
             "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr",
+            "formatter": "color",
+            "stream": "ext://sys.stdout",
         },
     },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+        "propagate": False,
+    },
     "loggers": {
-        name: {
-            "handlers": ["default"],
-            "level": "INFO",
-        }
-        for name in ["app", "tasks", "workers", "events", "ws", "server_info", "celery_app", "lifespan"]
+        # Internal loggers
+        "app": {"level": "INFO"},
+        "tasks": {"level": "INFO"},
+        "workers": {"level": "INFO"},
+        "events": {"level": "INFO"},
+        "ws": {"level": "INFO"},
+        "server_info": {"level": "INFO"},
+        "celery_app": {"level": "INFO"},
+        "lifespan": {"level": "INFO"},
+
+        # Third-party loggers
+        "asyncio": {"level": "INFO"},
+        "concurrent": {"level": "INFO"},
+        "faker": {"level": "INFO"},
+        "amqp": {"level": "INFO"},
+        "celery": {"level": "INFO"},
+        "dotenv": {"level": "INFO"},
+        "fastapi": {"level": "INFO"},
+        "fastapi_cache": {"level": "INFO"},
+        "kombu": {"level": "INFO"},
     },
 }
