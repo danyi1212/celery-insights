76484d84407eabeda38a83a164741891dc8de31a
Dan Yishai <danyi1212@users.noreply.github.com>
2023-10-05 19:32:32 +0300

    Changed logging config to dict

--- server/logging_config.py
+++ server/logging_config.py
@@ -1,29 +1,27 @@
-from pydantic import BaseModel
-
 from settings import Settings
 
-
-class LoggingConfig(BaseModel):
-    version = 1
-    disable_existing_loggers = False
-    formatters = {
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
         "default": {
             "()": "uvicorn.logging.DefaultFormatter",
             "fmt": "[%(asctime)s.%(msecs)03d] %(levelname)s - %(module)s:%(lineno)d | %(message)s",
             'datefmt': "%H:%M:%S",
         },
-    }
-    handlers = {
+    },
+    "handlers": {
         "default": {
             "formatter": "default",
             "class": "logging.StreamHandler",
             "stream": "ext://sys.stderr",
         },
-    }
-    loggers = {
+    },
+    "loggers": {
         name: {
             "handlers": ["default"],
             "level": Settings().log_level,
         }
         for name in ["app", "tasks", "workers", "events", "ws", "server_info", "celery_app", "lifespan"]
-    }
+    },
+}
