0b58a3fdce2f3c7146f227934e9037d0f911c3fb
Dan Yishai <danyi1212@users.noreply.github.com>
2023-11-19 15:48:39 +0200

    Added server info to debug bundle

--- server/server_info/debug_bundle.py
+++ server/server_info/debug_bundle.py
@@ -10,7 +10,7 @@ from starlette.requests import Request
 
 from events.receiver import state
 from logging_config import LOG_FILE_PATH
-from server_info.models import ClientDebugInfo, StateDump
+from server_info.models import ClientDebugInfo, ServerInfo, StateDump
 from settings import Settings
 from tasks.model import Task
 from workers.models import Worker
@@ -36,7 +36,7 @@ async def dump_file(file: zipfile.ZipFile, filename: str, path: AsyncPath) -> No
 
 async def generate_bundle_file(settings: Settings, browser: UserAgentInfo | None,
                                client_info: ClientDebugInfo, connections: list[ClientInfo],
-                               state_dump: StateDump) -> BytesIO:
+                               state_dump: StateDump, server_info: ServerInfo) -> BytesIO:
     buffer = BytesIO()
     with zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED) as file:
         async with asyncio.TaskGroup() as tg:
@@ -44,9 +44,10 @@ async def generate_bundle_file(settings: Settings, browser: UserAgentInfo | None
             tg.create_task(dump_file(file, "app.log", AsyncPath(LOG_FILE_PATH)))
             dump_model(file, "settings.json", settings)
             dump_model(file, "browser.json", browser)
-            dump_model(file, "client.json", client_info)
+            dump_model(file, "client_info.json", client_info)
             dump_model(file, "connections.json", connections)
             dump_model(file, "state.json", state_dump)
+            dump_model(file, "server_info.json", server_info)
 
     buffer.seek(0)
     return buffer
@@ -75,4 +76,5 @@ async def create_debug_bundle(request: Request, client_info: ClientDebugInfo) ->
     browser = get_user_agent(request)
     connections = list(events_manager.get_clients())
     state_dump = get_state_dump()
-    return await generate_bundle_file(settings, browser, client_info, connections, state_dump)
+    server_info = ServerInfo.create(request, state)
+    return await generate_bundle_file(settings, browser, client_info, connections, state_dump, server_info)
