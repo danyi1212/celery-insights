74e8a81574da5d98e46018d1a04def9d84d8b13f
Dan Yishai <danyi1212@users.noreply.github.com>
2023-11-19 15:15:17 +0200

    Added loading indicator for download debug bundle button

--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -1,40 +1,11 @@
+import { DownloadDebugBundleButton } from "@components/settings/DownloadDebugBundleButton"
 import OnlineClientsPanel from "@components/settings/OnlineClientsPanel"
 import { ServerInfoPanel } from "@components/settings/ServerInfoPanel"
 import SettingsPanel from "@components/settings/SettingsPanel"
-import Button from "@mui/material/Button"
 import Grid from "@mui/material/Grid"
-import useSettingsStore from "@stores/useSettingsStore"
 import React from "react"
 
-const handleDownloadDebugBundle = async () => {
-    const response = await fetch("/api/settings/download-debug-bundle", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            settings: useSettingsStore.getState(),
-            screen_height: window.innerHeight,
-            screen_width: window.innerWidth,
-        }),
-    })
-    if (!response.ok) return
-
-    const blob = await response.blob()
-    const blobUrl = window.URL.createObjectURL(blob)
-
-    const anchor = document.createElement("a")
-    anchor.href = blobUrl
-    anchor.download = "debug_bundle.zip"
-    document.body.appendChild(anchor)
-    anchor.click() // Programmatically click the anchor to trigger the download
-
-    document.body.removeChild(anchor)
-    URL.revokeObjectURL(blobUrl)
-}
-
 const SettingsPage: React.FC = () => {
-    const isDemo = useSettingsStore((state) => state.demo)
     return (
         <Grid container spacing={3} px={3}>
             <Grid item xs={12} md={6} lg={8}>
@@ -45,15 +16,7 @@ const SettingsPage: React.FC = () => {
                 <OnlineClientsPanel />
             </Grid>
             <Grid item xs={12} justifyContent="center" alignItems="center">
-                <Button
-                    variant="outlined"
-                    color="secondary"
-                    sx={{ mx: 5 }}
-                    onClick={handleDownloadDebugBundle}
-                    disabled={isDemo}
-                >
-                    Download Debug Bundle
-                </Button>
+                <DownloadDebugBundleButton sx={{ mx: 5 }} />
             </Grid>
         </Grid>
     )
